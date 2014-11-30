bb8585995c5437e064cd35db85ba0f326b7e9ba6
Devrandom <c1.devrandom@niftybox.net>
2014-11-30 11:24:21 -0800

    reorganize script vs package

--- digitaloracle/oracle.py
+++ digitaloracle/oracle.py
@@ -10,7 +10,9 @@ from pycoin.serialize import b2h, h2b
 from pycoin.key import Key
 from pycoin.key.BIP32Node import BIP32Node
 from pycoin.networks import full_network_name_for_netcode, network_name_for_netcode, NETWORK_NAMES
-from pycoin.tx.pay_to import ScriptMultisig
+from pycoin.tx.pay_to import ScriptMultisig, build_p2sh_lookup
+from pycoin.tx.script.tools import *
+from pycoin.tx.script import der
 import json
 import requests
 import uuid
@@ -25,6 +27,27 @@ class Oracle(object):
     def __init__(self, keys, manager=None):
         self.keys = keys
         self.manager = manager
+        self.pubkeys = [str(key.wallet_key(as_private=False)) for key in self.keys]
+
+    def sign(self, tx):
+        replace_dummy(tx)
+        # sign!
+
+    def url(self):
+        account_id = str(uuid.uuid5(uuid.NAMESPACE_URL, "urn:digitaloracle.co:%s"%(self.pubkeys[0])))
+        url = 'https://s.digitaloracle.co/keychains/' + account_id
+        return url
+
+    def get(self):
+        url = self.url()
+        response = requests.get(url)
+        result = response.json()
+        if response.status_code == 200 and result.get('result', None) == 'success':
+            self.oracle_keys = [BIP32Node.from_hwif(s) for s in result['keys']['default']]
+        elif response.status_code == 200 or response.status_code == 400:
+            raise OracleError(response.content)
+        else:
+            raise Error("Unknown response " + response.status_code)
 
     def create(self, email):
         r = {}
@@ -41,24 +64,50 @@ class Oracle(object):
         r['pii'] = {
             'email': email,
         }
-        pubkeys = [str(key.wallet_key(as_private=False)) for key in self.keys]
-        r['keys'] = pubkeys
-        account_id = str(uuid.uuid5(uuid.NAMESPACE_URL, "urn:digitaloracle.co:%s"%(pubkeys[0])))
+        r['keys'] = self.pubkeys
         body = json.dumps(r)
-        url = 'https://s.digitaloracle.co/keychains/' + account_id
+        url = self.url()
         response = requests.post(url, body, headers={'content-type': 'application/json'})
 
         if response.status_code != 200:
             raise Error("Error contacting oracle")
 
         result = response.json()
-        if result.get('result', None) == 'success':
-            oracle_keys = [BIP32Node.from_hwif(s) for s in result['keys']['default']]
-            return oracle_keys
-        elif result.get('error', None) == 'already exists':
-            response = requests.get(url)
+        if response.status_code == 200 and result.get('result', None) == 'success':
+            self.oracle_keys = [BIP32Node.from_hwif(s) for s in result['keys']['default']]
+        elif response.status_code == 400 and result.get('error', None) == 'already exists':
+            raise OracleError("already exists")
+        elif response.status_code == 200 or response.status_code == 400:
             raise OracleError(response.content)
         else:
-            raise Error("Unknown response")
+            raise Error("Unknown response " + response.status_code)
+
+    def all_keys(self):
+        if self.oracle_keys :
+            return self.keys + self.oracle_keys
+        else:
+            raise OracleError("oracle_keys not initialized - get, create, or set the property")
+
+    def script(self, path):
+        subkeys = [key.subkey_for_path(path or "") for key in self.all_keys()]
+        secs = [key.sec() for key in subkeys]
+        secs.sort()
+        script = ScriptMultisig(2, secs)
+        return script
 
 
+def dummy_signature(sig_type):
+    order = generator_secp256k1.order()
+    r, s = order - 1, order // 2
+    return der.sigencode_der(r, s) + bytes_from_int(sig_type)
+
+def replace_dummy(tx):
+    """replace dummy signatures with OP_0 for digitaloracle compatibility"""
+    dummy = b2h(dummy_signature(1))
+    for inp in tx.txs_in:
+        ops1 = []
+        for op in opcode_list(inp.script):
+            if op == dummy:
+                op = 'OP_0'
+            ops1.append(op)
+        inp.script = compile(' '.join(ops1))
--- digitaloracle/scripts/do.py
+++ digitaloracle/scripts/do.py
@@ -2,7 +2,7 @@
 
 from __future__ import print_function
 
-import pdb
+from pdb import set_trace
 import sys
 import os
 import argparse
@@ -18,39 +18,11 @@ from pycoin.tx.tx_utils import LazySecretExponentDB
 import json
 import requests
 import uuid
+from digitaloracle import Oracle
 
 def sign(tx, script, key):
-    #pdb.set_trace()
     lookup = build_p2sh_lookup([script.script()])
     tx.sign(LazySecretExponentDB([key.wif()], {}), p2sh_lookup=lookup)
-
-def create(keys):
-    r = {}
-    r['walletAgent'] = 'digitaloracle-pycoin-0.01'
-    r['rulesetId'] = 'default'
-    r['parameters'] = {
-            "levels": [ { "asset": "BTC", "period": 60, "value": 0.001 },
-                { "delay": 0, "calls": ['email'] } ]
-            }
-    r['pii'] = { 'email': 'c1.github@niftybox.net' }
-    pubkeys = [str(key.wallet_key(as_private=False)) for key in keys]
-    r['keys'] = pubkeys
-    account_id = str(uuid.uuid5(uuid.NAMESPACE_URL, "urn:digitaloracle.co:%s"%(pubkeys[0])))
-    body = json.dumps(r)
-    url = 'https://s.digitaloracle.co/keychains/' + account_id
-    response = requests.post(url, body, headers={'content-type': 'application/json'})
-    #print(response.status_code)
-    result = response.json()
-    if result.get('result', None) == 'success':
-        print('success')
-    elif result.get('error', None) == 'already exists':
-        print('exists')
-        response = requests.get(url)
-        result = response.json()
-    #print(result)
-    oracle_keys = [BIP32Node.from_hwif(s) for s in result['keys']['default']]
-    keys = keys + oracle_keys
-    return keys
 
 def main():
     parser = argparse.ArgumentParser(
@@ -90,14 +62,17 @@ def main():
                         except Exception as ex:
                             pass
                         continue
-                except Exception:
+                except Exception as ex:
+                    print('could not parse %s %s' %(item, ex), file=sys.stderr)
                     pass
+
+    oracle = Oracle(keys)
 
     if args.command == 'dump':
         for key in keys:
             print(key.wallet_key(as_private=False))
     elif args.command == 'create':
-        keys = create(keys)
+        oracle.create()
         subkeys = [key.subkey_for_path(args.subkey or "") for key in keys]
         for key in subkeys:
             print(key.wallet_key(as_private=False))
@@ -107,19 +82,18 @@ def main():
         script = ScriptMultisig(2, secs)
         print(script.address(netcode=args.network))
     elif args.command == 'sign':
-        keys = create(keys)
-        subkeys = [key.subkey_for_path(args.subkey or "") for key in keys]
-        secs = [key.sec() for key in subkeys]
-        secs.sort()
-        script = ScriptMultisig(2, secs)
+        oracle.get()
+        script = oracle.script(args.subkey)
         print(script.address(netcode=args.network))
         for tx in txs:
             print(tx.id())
             print(b2h(stream_to_bytes(tx.stream)))
-            print('scripts:')
-            for inp in tx.txs_in:
-                print('- %s'%(inp.script))
-            sign(tx, script, subkeys[0])
+#            print('scripts:')
+#            for inp in tx.txs_in:
+#                print('- %s'%(inp.script))
+            subkey = keys[0].subkey_for_path(args.subkey or "")
+            sign(tx, script, subkey)
+            oracle.sign(tx)
             print(b2h(stream_to_bytes(tx.stream)))
 
     else:
